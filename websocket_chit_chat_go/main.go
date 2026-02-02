7cdb8015740fd5f38bedf2fcca9e2a3920ccad75
Mathieu Cizo <maciz@outlook.fr>
2026-02-02 20:39:11 +0100

    Fix Websocket chit chat
    Add TCP latency measurement and average computation to WebSocket client

--- websocket_chit_chat_go/main.go
+++ websocket_chit_chat_go/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"container/ring"
 	"context"
 	"fmt"
 	"io"
 	"log"
 	"math"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -23,11 +25,15 @@ func main() {
 	if token == "" {
 		panic("HACKATTIC_TOKEN environment variable not set")
 	}
-	fmt.Println("Hello World")
 
 	// Keep the program alive until user interrupts (Ctrl+C) or the process is terminated.
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
+
+	avgRequestCh := make(chan struct{}, 1)
+	avgResultCh := make(chan time.Duration)
+	go serveLatencyAverage(ctx, "hackattic.com", avgRequestCh, avgResultCh)
+	time.Sleep(4 * time.Second)
 
 	wsToken := getWebsocketToken(token)
 	c, resp, err := websocket.Dial(ctx, WSBaseURL+wsToken, nil)
@@ -42,6 +48,7 @@ func main() {
 
 	for {
 		msgType, data, err := c.Read(ctx)
+
 		if err != nil {
 			if websocket.CloseStatus(err) == websocket.StatusNormalClosure {
 				log.Println("Connection closed normally")
@@ -52,7 +59,7 @@ func main() {
 		go func() {
 			log.Printf("msgType=%v, data=%s\n", msgType, data)
 			if string(data) == "ping!" {
-				interval := computeInterval(pingTime)
+				interval := computeInterval(pingTime, avgRequestCh, avgResultCh)
 				pingTime = time.Now()
 				err = c.Write(ctx, websocket.MessageText, strconv.AppendInt([]byte{}, interval, 10))
 				if err != nil {
@@ -61,16 +68,16 @@ func main() {
 				log.Printf("\t=> sent interval %d\n", interval)
 			}
 		}()
+
 	}
-	//submitSolution(token, "zut")
 }
 
 func logHandshake(resp *http.Response) {
 	log.Printf("Handshake response status: %s\n", resp.Status)
 	log.Printf("Handshake response headers: %v\n", resp.Header)
 	if resp.Body != nil {
-		body, err := io.ReadAll(resp.Body)
 		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
 		if err != nil {
 			panic(err)
 		}
@@ -83,8 +90,13 @@ type intervalDiff struct {
 	diff     float64
 }
 
-func computeInterval(pingTime time.Time) int64 {
+func computeInterval(pingTime time.Time, requestCh chan struct{}, resultCh chan time.Duration) int64 {
+	requestCh <- struct{}{}
+	lat := <-resultCh
+	log.Println("computeInterval() Received avg latency:", lat.Milliseconds(), "ms")
 	duration := time.Since(pingTime).Milliseconds()
+	log.Println("computeInterval() raw Duration since last ping:", duration, "ms")
+	duration = duration - lat.Milliseconds()
 	intervals := []int64{700, 1500, 2000, 2500, 3000}
 	bestInterval := intervalDiff{diff: math.MaxFloat64}
 	for _, interval := range intervals {
@@ -94,4 +106,66 @@ func computeInterval(pingTime time.Time) int64 {
 		}
 	}
 	return bestInterval.interval
+}
+
+func measureTCPDialDuration(host string, port int) (time.Duration, error) {
+	start := time.Now()
+	conn, err := net.DialTimeout("tcp", host+":"+strconv.Itoa(port), 2*time.Second)
+	if err != nil {
+		return 0, fmt.Errorf("failed to connect to %s:%d: %w", host, port, err)
+	}
+	defer conn.Close()
+	//log.Println("TCP connection established in", time.Since(start).Milliseconds(), "ms")
+	return time.Since(start), nil
+}
+
+func serveLatencyAverage(ctx context.Context, host string, request chan struct{}, result chan time.Duration) {
+	ticker := time.NewTicker(300 * time.Millisecond)
+	r := ring.New(12)
+	latency, err := measureTCPDialDuration(host, 80)
+	if err != nil {
+		panic(err)
+	}
+	r.Value = latency
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			latency, err = measureTCPDialDuration(host, 80)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			r = r.Next()
+			r.Value = latency
+		case <-request:
+			avg := avgDurationRing(r)
+			log.Println("Average latency:", avg)
+			result <- avg
+		}
+	}
+}
+
+func avgDurationRing(r *ring.Ring) time.Duration {
+	if r == nil || r.Len() == 0 {
+		return 0
+	}
+	var sum time.Duration
+	var count int64
+
+	r.Do(func(v any) {
+		if v == nil {
+			return
+		}
+		if lat, ok := v.(time.Duration); ok {
+			sum += lat
+			count++
+		}
+	})
+	if count == 0 {
+		return 0
+	}
+	return sum / time.Duration(count)
 }
